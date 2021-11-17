674357a4c6f4f64f9c4d9cd0abefddcebcdd92ad
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-11-16 22:16:32 -0500

    cookiecutter-django: Add instructions for adding Fathom to templates

--- cookiecutter-django/hooks/post_gen_project.py
+++ cookiecutter-django/hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from textwrap import dedent
 
 
 # https://github.com/django/django/blob/stable/3.2.x/django/core/management/utils.py get_random_secret_key
@@ -19,7 +20,23 @@ def set_secret_key(path):
 def main():
     set_secret_key(os.path.join("core", "settings.py"))
 
-    if "{{ cookiecutter.use_fathom }}".lower() != "y":
+    if "{{ cookiecutter.use_fathom }}".lower() == "y":
+        print(dedent(
+            {%- raw %}
+            """
+            Run python manage.py startapp and add a fathom.html file to its templates/ directory:
+
+            {% if fathom.id %}
+            <script src="https://{{ fathom.domain }}/script.js" data-site="{{ fathom.id }}" defer data-excluded-domains="127.0.0.1,0.0.0.0"></script>
+            {% endif %}
+
+            Add before the end of the <head> element in your base template:
+
+            {% include 'fathom.html' %}
+            """
+            {%- endraw %}
+        ))
+    else:
         os.remove(os.path.join("core", "context_processors.py"))
 
 
