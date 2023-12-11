5db38a82d2589e64c66b5312008b67617b05d981
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-12-11 18:27:52 -0500

    python/django: Use variable name that is unlikely to conflict with module name

--- cookiecutter-django/{{cookiecutter.project_slug}}/core/urls.py
+++ cookiecutter-django/{{cookiecutter.project_slug}}/core/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path
 
-sitemaps = {}
+maps = {}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
+    path("sitemap.xml", sitemap, {"sitemaps": maps}, name="django.contrib.sitemaps.views.sitemap"),
 ]
