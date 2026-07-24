2b041feefb4a325c05ddc540168a0b29c28ccd63
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-07-23 21:52:52 -0400

    chore: Fix PTH checks

--- cookiecutter-django/{{cookiecutter.project_slug}}/core/settings.py
+++ cookiecutter-django/{{cookiecutter.project_slug}}/core/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
-from glob import glob
 from pathlib import Path
 
 import dj_database_url
@@ -138,7 +137,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Project-specific Django configuration
 
-LOCALE_PATHS = glob(str(BASE_DIR / "**" / "locale"))
+LOCALE_PATHS = list(BASE_DIR.glob("*/locale"))
 
 STATIC_ROOT = BASE_DIR / "static"
 {%- if cookiecutter.use_fathom == "y" %}
